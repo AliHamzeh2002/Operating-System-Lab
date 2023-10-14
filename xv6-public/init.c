b9181facb367258aa7f76798def0862372a3899a
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-10-14 18:42:19 +0330

    Modify boot message

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -21,6 +21,7 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "Group #9\n1. Narges Haeri\n2. Ali Hamzehpour\n3. Mina Shirazi\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
