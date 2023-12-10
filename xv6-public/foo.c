9807442cee57e84998175d34eeac90569af6b49d
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-12-10 09:47:48 +0330

    Add foo program

---
+++ xv6-public/foo.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "user.h"
+
+
+#define NUM_FORKS 10
+int main(int argc, char* argv[]){
+    for (int i = 0; i < NUM_FORKS; i++){
+        int pid = fork();
+        if (pid == 0){
+            sleep(4000);
+            int x = 0;
+            for (long long j = 0; j < 1000000000000; j++){
+                for (long long k = 0; k < 10000000000000; k++){
+                    x += k * 12 - j;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < NUM_FORKS; i++){
+        wait();
+    }
+}
